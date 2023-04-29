764fda09851402903ca6cb6ff7dcc9b54128a13c
Alastors <78517796+Alastors@users.noreply.github.com>
2023-04-29 02:26:17 -0400

    Revert "Update Config.java"
    This reverts commit 806c326b6217bdb8439b6820a93bf43b2eabcab5.

--- src/main/java/magicbees/main/Config.java
+++ src/main/java/magicbees/main/Config.java
@@ -3,7 +3,6 @@ package magicbees.main;
 import java.io.File;
 import java.lang.reflect.Field;
 
-import cpw.mods.fml.common.Loader;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
@@ -210,8 +209,6 @@ public class Config {
         magicCapsule = new ItemCapsule(CapsuleType.MAGIC, capsuleStackSizeMax);
         voidCapsule = new ItemCapsule(CapsuleType.VOID, capsuleStackSizeMax);
 
-        bloodMagicActive = Loader.isModLoaded("AWWayofTime");
-
         setupThaumcraftItems();
         setupBotaniaItems();
 
@@ -254,7 +251,7 @@ public class Config {
         arsMagicaActive = p.getBoolean();
 
         p = configuration.get(CATEGORY_MODULES, "BloodMagic", true);
-       // bloodMagicActive = p.getBoolean();
+        bloodMagicActive = p.getBoolean();
 
         p = configuration.get(CATEGORY_MODULES, "EquivalentExchange", true);
         equivalentExchangeActive = p.getBoolean();
