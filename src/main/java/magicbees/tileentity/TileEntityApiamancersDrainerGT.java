4823e447939b09973b3dbdad4e494b4612140086
Quantumlyy <ethoarx@gmail.com>
2023-12-13 12:04:15 +0100

    add some scanner data

--- src/main/java/magicbees/tileentity/TileEntityApiamancersDrainerGT.java
+++ src/main/java/magicbees/tileentity/TileEntityApiamancersDrainerGT.java
@@ -1,16 +1,44 @@
 package magicbees.tileentity;
 
+import java.util.stream.Collectors;
+
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumChatFormatting;
 
 import cpw.mods.fml.common.Loader;
-import forestry.api.apiculture.*;
+import forestry.api.apiculture.IBeeHousing;
 import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
+import gregtech.api.interfaces.tileentity.IGregTechDeviceInformation;
 import gregtech.api.metatileentity.BaseMetaTileEntity;
 import gregtech.common.tileentities.machines.basic.GT_MetaTileEntity_IndustrialApiary;
 
-public class TileEntityApiamancersDrainerGT extends TileEntityApiamancersDrainerCommon {
+public class TileEntityApiamancersDrainerGT extends TileEntityApiamancersDrainerCommon
+        implements IGregTechDeviceInformation {
 
     public static boolean isGTLoaded = Loader.isModLoaded("gregtech");
+
+    @Override
+    public boolean isGivingInformation() {
+        return true;
+    }
+
+    @Override
+    public String[] getInfoData() {
+        String aspects = essentia.aspects.entrySet().stream()
+                .map(
+                        e -> EnumChatFormatting.LIGHT_PURPLE + e.getKey().getName()
+                                + ": "
+                                + EnumChatFormatting.GREEN
+                                + e.getValue())
+                .collect(Collectors.joining(EnumChatFormatting.RESET + ", " + EnumChatFormatting.RESET));
+
+        return new String[] { //
+                EnumChatFormatting.DARK_PURPLE + "Apiamancer's Drainer" + EnumChatFormatting.RESET, //
+                aspect != null ? "Attuned: " + aspect.getName() : "Not attuned", //
+                "Stored Essentia:", //
+                aspects //
+        };
+    }
 
     @Override
     protected IBeeHousing beeHousing(TileEntity above) {
