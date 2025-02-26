81cb972fff5b6e5fa0d34e7972b7480309afe038
Maya <10861407+serenibyss@users.noreply.github.com>
2025-02-25 21:11:41 -0600

    update

--- src/main/java/magicbees/main/utils/VersionInfo.java
+++ src/main/java/magicbees/main/utils/VersionInfo.java
@@ -1,5 +1,7 @@
 package magicbees.main.utils;
 
+import magicbees.Tags;
+
 /**
  * Using some of the VersionInfo code from cofh. (Thanks, Lemming!)
  *
@@ -8,7 +10,7 @@ package magicbees.main.utils;
 public class VersionInfo {
 
     public static final String ModName = "MagicBees";
-    public static final String Version = "GRADLETOKEN_VERSION";
+    public static final String Version = Tags.VERSION;
     public static final String GUI_FACTORY_CLASS = "magicbees.client.gui.GuiFactory";
     public static final String Logo = "/gfx/magicbees/logo.png";
     public static final String Depends = "required-after:Forestry@[4.2.5,);after:Baubles;after:Thaumcraft;after:ExtraBees;after:EE3;after:ArsMagica;after:TConstruct;after:Railcraft;after:ThermalFoundation;after:ThermalExpansion;after:RedstoneArsenal;after:AWWayofTime;after:Botania;after:appliedenergistics2";
